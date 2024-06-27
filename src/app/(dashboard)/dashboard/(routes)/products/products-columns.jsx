a26b2e42a0ccb43b7c558056e12d14e017f1b6dd
Harsh Kapil <kapilharsh1999@gmail.com>
2024-06-27 17:37:05 +0530

    -Products Table's Columns desiging

--- src/app/(dashboard)/dashboard/(routes)/products/products-columns.jsx
+++ src/app/(dashboard)/dashboard/(routes)/products/products-columns.jsx
@@ -1,11 +1,32 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { cn } from "@/lib/utils";
+import { ArrowUpDown, Eye, Pen, Trash2 } from "lucide-react";
+
 
 
 export const columns = [
     {
         accessorKey: "id",
-        header: "ID",
+        header: ({ table }) => (
+            <Checkbox
+              checked={table.getIsAllRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
+              onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+              aria-label="Select all"
+            />
+          ),
+          cell: ({ row }) => {
+            const user = row.original;
+            return (
+              <Checkbox
+              checked={row.getIsSelected()}
+              onCheckedChange={(value) => row.toggleSelected(!!value)}
+              aria-label="Select row"
+            />
+            )
+          }
     },
     {
         accessorKey: "name",
@@ -26,14 +47,52 @@ export const columns = [
     {
         accessorKey: "status",
         header: "Status",
+        cell: ({ row }) => {
+            const status = row.getValue("status");
+            return (
+                <div className={cn(
+                    "font-medium p-0.5 flex items-center justify-center rounded-full",
+                     status === "active" ? "text-green-600 bg-green-100" : "text-gray-600 bg-gray-100"
+                     )}>
+                    {status}
+                </div>
+            )
+        }
     },
     {
         accessorKey: "date",
-        header: "Date",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Date
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+          cell: ({ row }) => {
+            const date = row.getValue("date");
+            return (
+                <>
+                    {new Date(date).toLocaleDateString()}
+                </>
+            )
+        }
     },
     {
         accessorKey: "action",
         header: "Action",
+        cell: ({ row }) => {
+            const user = row.original;
+            return (
+              <div className="flex gap-0.5">
+                <Button variant="ghost" className="p-1"><Eye className="w-6 h-6 p-1" /></Button>
+                <Button variant="ghost" className="p-1"><Pen className="w-6 h-6 p-1" /></Button>
+                <Button variant="ghost" className="p-1"><Trash2 className="w-6 h-6 p-1" /></Button>
+              </div>
+            )
+        }
     }
-
 ]
--- src/components/ui/data-table.jsx
+++ src/components/ui/data-table.jsx
@@ -60,7 +60,7 @@ export function DataTable({
 
   return (
     <>
-    <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between">
                 <Input 
                     placeholder="Search products..." 
                     className="w-1/4" 
@@ -73,51 +73,50 @@ export function DataTable({
                 <Button variant="outline"><SlidersHorizontal className="w-8 h-8 p-2" /> Filter</Button>
                 </div>
             </div>
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
+        <div className="rounded-md border bg-white">
+            <Table>
+                <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                        return (
+                        <TableHead key={header.id}>
+                            {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                                )}
+                        </TableHead>
+                        )
+                    })}
+                    </TableRow>
                 ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      
-    </div>
+                </TableHeader>
+                <TableBody>
+                {table.getRowModel().rows?.length ? (
+                    table.getRowModel().rows.map((row) => (
+                    <TableRow
+                        key={row.id}
+                        data-state={row.getIsSelected() && "selected"}
+                    >
+                        {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </TableCell>
+                        ))}
+                    </TableRow>
+                    ))
+                ) : (
+                    <TableRow>
+                    <TableCell colSpan={columns.length} className="h-24 text-center">
+                        No results.
+                    </TableCell>
+                    </TableRow>
+                )}
+                </TableBody>
+            </Table>
+        </div>
     <DataTablePagination table={table} />
     </>
   )
