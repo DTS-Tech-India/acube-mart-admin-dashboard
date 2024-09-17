4183cc138374660b7db72de2c6a17397fda1ca00
Harsh Kapil <kapilharsh1999@gmail.com>
2024-09-17 11:42:22 +0530

    -transactions page fix for null order id.

--- src/app/(dashboard)/(routes)/transactions/page.jsx
+++ src/app/(dashboard)/(routes)/transactions/page.jsx
@@ -36,7 +36,7 @@ export default function Transactions() {
           return {
             id: transaction._id,
             transactionId: transaction._id,
-            orderId: transaction.orderId._id,
+            orderId: transaction?.orderId?._id,
             userId: transaction.userId._id,
             paymentMode: transaction.paymentMode,
             amount: transaction.amount,
