1e09ca717fe9a3c0597cc278c7c8745b13228e43
Harsh Kapil <kapilharsh1999@gmail.com>
2024-09-23 10:50:21 +0530

    -push changes that last lime was made

--- src/components/line-chart.jsx
+++ src/components/line-chart.jsx
@@ -21,23 +21,12 @@ import { useQuery } from "@tanstack/react-query"
 import { useMemo } from "react"
 import { Skeleton } from "./ui/skeleton"
 export const description = "A line chart with dots"
-const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-]
+
 const chartConfig = {
   amount: {
     label: "Sales",
     color: "hsl(var(--chart-1))",
-  },
-  mobile: {
-    label: "Mobile",
-    color: "hsl(var(--chart-2))",
-  },
+  }
 }
 
 export function LineChartComponent() {
