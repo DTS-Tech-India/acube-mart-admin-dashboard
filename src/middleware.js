ed5259f60b38150013a2ea86b45b414ee0b413ec
Harsh Kapil <kapilharsh1999@gmail.com>
2024-09-24 10:00:03 +0530

    -middleware update test for aws deployment middleware issue.

--- src/middleware.js
+++ src/middleware.js
@@ -3,24 +3,24 @@ import { NextResponse } from 'next/server'
 import { getSession, updateSession } from './lib/session'
 
  //const superAdminRoutes = ['/dashboard/admin'] 
- const adminRoutes = ['/admins', '/settings']
+ //const adminRoutes = ['/admins', '/settings']
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
     
-    const path = request.nextUrl.pathname
+    //const path = request.nextUrl.pathname
     //console.log(path)
     
-    const session = await getSession()
+    //const session = await getSession()
     
-    if(!session) return NextResponse.redirect(new URL('/signin', request.url))
+    //if(!session) return NextResponse.redirect(new URL('/signin', request.url))
 
-    const role = await session.admin.role
+    //const role = await session.admin.role
     //console.log(role);
 
     //if(role === 'admin' && superAdminRoutes.includes(path)) return NextResponse.redirect(new URL('/unauthorized', request.url))
     
-    if(role === 'manager' && (/* superAdminRoutes.includes(path) || */ adminRoutes.includes(path))) return NextResponse.redirect(new URL('/unauthorized', request.url))
+    //if(role === 'manager' && (/* superAdminRoutes.includes(path) || */ adminRoutes.includes(path))) return NextResponse.redirect(new URL('/unauthorized', request.url))
 
   return NextResponse.next()
 }
