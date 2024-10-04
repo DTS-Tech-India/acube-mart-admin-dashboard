83d860e56e1ee1a40bcba04081e65ad00089f370
Harsh Kapil <kapilharsh1999@gmail.com>
2024-10-04 11:49:34 +0530

    -middleware debugging.

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,24 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
-import { getSession, updateSession } from './lib/session'
+//import { getSession, updateSession } from './lib/session'
+import getUserRole from './lib/session'
  
 export async function middleware(request) {
     
     const path = request.nextUrl.pathname
     //console.log(path)
     
-    const session = await getSession()
+    //const session = await getSession()
+    //console.log(session)
     
-    if(!session) return NextResponse.redirect(new URL('/signin', request.url))
+    //if(!session) return NextResponse.redirect(new URL('/signin', request.url))
 
-    const role = await session.admin.role
+    //const role = await session.admin.role
     //console.log(role);
+    const role = await getUserRole()
+    //console.log(role);
+
+    if (!role) return NextResponse.redirect(new URL('/signin', request.url))
 
     if (role === 'manager' && (path.startsWith('/admins') || path.startsWith('/settings'))) return NextResponse.redirect(new URL('/unauthorized', request.url))
 
