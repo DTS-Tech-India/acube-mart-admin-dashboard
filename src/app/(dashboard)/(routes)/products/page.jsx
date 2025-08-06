cfd68e5691bb7e45d0a4703d90928a696f4b6d45
Harsh Kapil <kapilharsh1999@gmail.com>
2025-08-06 15:30:14 +0530

    -Update Products to Google Merchant Center implementation.

--- src/app/(dashboard)/(routes)/products/page.jsx
+++ src/app/(dashboard)/(routes)/products/page.jsx
@@ -18,13 +18,15 @@ import { useRouter } from "next/navigation";
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "./products-columns";
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { products } from "@/lib/get-api-data";
 import axios from "axios";
+import { toast } from "sonner";
 
 export default function Products() {
     const router = useRouter()
+    const[loading, setLoading] = useState(false);
     const { data, isLoading, isError } = useQuery({
         queryKey: ["products"],
         queryFn: async() => await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/product/all`).then((res) => res.data),
@@ -50,10 +52,26 @@ export default function Products() {
     /* console.log(data)
     console.log(modifiedData) */
 
+    const handleUpdateProductsToGoogleMerchantCenter = async () => {
+        setLoading(true);
+        axios.put(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/googlesheetproducts/update`)
+            .then((res) => {
+                //console.log(res);
+                toast.success("Products updated to Google Merchant Center successfully.");
+            })
+            .catch((err) => {
+                console.error(err);
+                toast.error("An error occurred while updating products to Google Merchant Center.");
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }
+
     return (
         <div className="w-full h-full flex flex-col gap-4">
             <h1 className="text-2xl font-semi">Products</h1>
-            <header className="flex items-center justify-between">
+            <header className="w-full flex items-center justify-between">
                 <Breadcrumb>
                     <BreadcrumbList>
                         <BreadcrumbItem>
@@ -73,6 +91,7 @@ export default function Products() {
                     <Button onClick={() => {router.push("/products/bulk-edit-products")}}>Bulk Edit Products</Button>
                 </div>
             </header>
+            <Button onClick={handleUpdateProductsToGoogleMerchantCenter} disabled={loading}>Update Products to Google Merchant Center</Button>
             {isLoading ? (
                 <Skeleton
                     className="h-96 w-full aspect-auto" 
